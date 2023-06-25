93597567d1fa0b64094748ded1c4b55c01f3a366
DevvIsme <mdatking123@gmail.com>
2023-06-25 16:06:48 +0700

    Thuoc tinh cua cac doi tuong (mau, co chu)

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Mau{
+  static const Color vien = Color(0xFFDADEF2) ;
+  static const Color btnbd = Color(0xFF005FD0);
+  static const double textsize = 15;
+  static const Color textcolor = Color(0xFF505673);
+}
